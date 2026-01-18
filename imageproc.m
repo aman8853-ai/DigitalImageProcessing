521c7e040553566f940008e5b50dea5df949d398
Aman <amankanujiya66@gmail.com>
2026-01-18 22:02:51 +0530

    Update image file names in imageproc.m

--- imageproc.m
+++ imageproc.m
@@ -2,13 +2,13 @@ clc;
 clear all;
 close all;
 
-I=imread("_.jpeg");
+I=imread("ios.jpeg");
 imshow(I);
 figure
 Ig=rgb2gray(I);
 imshow(Ig);
 
-I_red=imread("Vidit.jpg");
+I_red=imread("ios.jpg");
 I_red(:,:,2)=0;
 I_red(:,:,3)=0;
 figure
